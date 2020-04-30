133b3b9878995a206cf9fee208cd885f571ac1a2
fherrera2 <59889160+fherrera2@users.noreply.github.com>
2020-04-30 10:30:52 -0500

    Add files via upload

---
+++ src/main/java/com/example/headstartstudentsignin/SignInSheet.java
@@ -0,0 +1,13 @@
+package com.example.headstartstudentsignin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignInSheet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_sheet);
+    }
+}
